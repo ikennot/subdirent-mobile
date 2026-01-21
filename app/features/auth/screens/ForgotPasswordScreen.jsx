9fee12fdd22ae814fd6dc520760fd06238c018ca
ikennot <domdomkenneth23@gmail.com>
2026-01-22 00:06:28 +0800

    add labels ang text in forgotPassword

--- app/features/auth/screens/ForgotPasswordScreen.jsx
+++ app/features/auth/screens/ForgotPasswordScreen.jsx
@@ -1,17 +1,49 @@
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet,Image} from 'react-native';
+import Logo from '@/assets/app-assets/logo.png'
+import LogoName from '@/assets/app-assets/logo-subdirent.png'
+import Button from "../components/button";
+import { Link } from "expo-router";
 
 export default function ForgotPasswordScreen() {
   return (
     <View style={styles.container}>
-      <Text>Forgot Password Screen</Text>
+     <Image source={Logo} style={styles.logo} />
+     <Image source = {LogoName} style = {styles.logoName} />
+     <Text style = {styles.headerText}>Forgot You Password?</Text>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+     flex:1,
+   justifyContent: 'center',
+   gap: 10,
+   alignItems:'center'
   },
+    logo: {
+        width: 100,
+        height: 100,
+        resizeMode: 'contain',
+        alignSelf: 'center',
+      marginBottom: 2,
+    },
+
+    logoName:{
+         width: 150,
+        height: 100,
+        resizeMode: 'contain',
+        alignSelf: 'center',
+        marginBottom: 20,
+    },
+    headerText: {
+    // The core styles matching the image:
+    fontSize:20,             
+    fontWeight: 'bold',       // Or '700'
+    color: '#102C4C',         // Deep Navy Blue
+    fontFamily: 'System',     // Uses San Francisco (iOS) or Roboto (Android)
+    textAlign: 'center',      // Usually centered on login screens
+    letterSpacing: 0.5,       // Adds a tiny bit of breathing room
+    marginBottom: 20,         // Spacing below the header
+  }
 });
