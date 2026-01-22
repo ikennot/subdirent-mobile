832b7fb29e46e41b705514cc5f848fe9fa38544a
ikennot <domdomkenneth23@gmail.com>
2026-01-22 16:16:26 +0800

    add some assets and some file components

---
+++ app/features/account/components/cards/CurrentInfoCard.jsx
@@ -0,0 +1,14 @@
+import { View, Text, StyleSheet} from 'react-native';
+
+
+
+export default function CurrentInfoCard(){
+    return (
+        <View>
+            <Text>
+                Nigga
+            </Text>
+        </View>
+    )
+    
+}
