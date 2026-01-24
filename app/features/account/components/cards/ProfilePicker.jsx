88a83ea981bb953d71cb3e194bcc5bddf5d010ec
ikennot <domdomkenneth23@gmail.com>
2026-01-24 21:27:35 +0800

    add expo-image-picker

---
+++ app/features/account/components/cards/ProfilePicker.jsx
@@ -0,0 +1,12 @@
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import * as ImagePicker from 'expo-image-picker';
+
+export default function ProfilePicker(){
+    return (
+        <View>
+
+        </View>
+    )
+}
+
