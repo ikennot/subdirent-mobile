fd44cc5344af7ece7c23cf1178d804f29bda2c94
ikennot <domdomkenneth23@gmail.com>
2026-01-26 04:24:53 +0800

    install relevant dependencies and add initial folder sructure for Navigator

--- app/index.jsx
+++ app/index.jsx
@@ -2,7 +2,7 @@ import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
 
 import LoginScreen from './features/auth/screens/LoginScreen';
 import Account from './features/account/screens/Account';
-
+import DashboardScreen from './features/dashboard/screens/DashboardScreen';
 // This file is the ENTRY SCREEN for Expo Router
 // Whatever you export here becomes the first screen
 
@@ -11,8 +11,9 @@ export default function Index() {
     <SafeAreaProvider>
       <SafeAreaView style={{ flex: 1 }}>
         {/* Palitan mo lang kung alin ang gusto mong first screen */}
-        {/* <LoginScreen /> */}
-        <Account />
+        <LoginScreen />
+        {/* <Account /> */}
+        {/* <DashboardScreen/> */}
       </SafeAreaView>
     </SafeAreaProvider>
   );
