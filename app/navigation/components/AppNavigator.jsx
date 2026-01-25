7407c40cb13f13c888dc46e99a357f4e728f955b
ikennot <domdomkenneth23@gmail.com>
2026-01-26 06:28:57 +0800

    some bug  in navigator

--- app/navigation/components/AppNavigator.jsx
+++ app/navigation/components/AppNavigator.jsx
@@ -1,9 +1,22 @@
 import  {View, StyleSheet,Image,Text} from "react-native";
 import Modules from "./buttons/Modules";
+import Dashboard from '../../../assets/navigation-assets/dashboard.png';
+import Property from '../../../assets/navigation-assets/property.png';
+import Payments from '../../../assets/navigation-assets/money.png';
+import Maintenance from '../../../assets/navigation-assets/maintenance.png';
+import Profile from '../../../assets/navigation-assets/profile.png';
+
+
+
 export default function AppNavigator(){
   return (
         <View style={styles.container}>
-          <Modules/>
+               <Modules icon={Dashboard} label="Dashboard" />
+      <Modules icon={Property} label="Property" />
+      <Modules icon={Payments} label="Payments" />
+      <Modules icon={Maintenance} label="Maintenance" />
+      <Modules icon={Profile} label="Profile" />
+
         </View>
     )
 }
@@ -12,8 +25,24 @@ export default function AppNavigator(){
 
 const styles = StyleSheet.create({
     container:{
-      width:'100%',
-      height:'70',
-      backgroundColor:'grey'
+    width: '100%',
+    height: 70,
+    borderRadius: 10,
+    backgroundColor: '#ffffff', // important for shadow and border visibility
+    borderWidth: 1,           // border
+    borderColor: '#748eed',      // light gray border
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignItems: 'center',
+
+    // Android shadow
+    elevation: 5,
+
+    // iOS shadow
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+
     }
 })
--- app/navigation/screens/AppNavigatorScreen.jsx
+++ app/navigation/screens/AppNavigatorScreen.jsx
@@ -1,17 +1,18 @@
-import  {View, StyleSheet,Image,Text} from "react-native";
+import { View, StyleSheet } from "react-native";
 import AppNavigator from "../components/AppNavigator";
-export default function AppNavigatorScreen(){
-return (
+
+export default function AppNavigatorScreen() {
+  return (
     <View style={styles.container}>
-        <AppNavigator/>
+      <AppNavigator />
     </View>
-)
+  );
 }
 
-
 const styles = StyleSheet.create({
-    container:{
-      justifyContent:'center',
-      alignItems:'center'
-    }
-})
+  container: {
+    flex: 1,                  // Make container take full screen height
+    justifyContent: 'flex-end', // Push content to bottom
+    alignItems: 'center',     // Center horizontally
+  },
+});
