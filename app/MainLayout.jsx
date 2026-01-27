f3cf3f9a930ffe6f8f00f7bf933c1ecb67fa666b
ikennot <domdomkenneth23@gmail.com>
2026-01-28 03:58:21 +0800

    integrate PaymentScreen

--- app/MainLayout.jsx
+++ app/MainLayout.jsx
@@ -5,7 +5,7 @@ import Account from "./features/account/screens/Account";
 import DashboardScreen from "./features/dashboard/screens/DashboardScreen";
 import MissingScreen from "./features/MissingScreen";
 import { SafeAreaView } from "react-native-safe-area-context";
-
+import PaymentScreen from "./features/payments/screens/PaymentScreen"
 const Tab = createBottomTabNavigator();
 
 export default function MainLayout() {
@@ -23,7 +23,7 @@ export default function MainLayout() {
         >
           <Tab.Screen name="Dashboard" component={DashboardScreen} />
           <Tab.Screen name="Property" component={MissingScreen} />
-          <Tab.Screen name="Payments" component={MissingScreen} />
+          <Tab.Screen name="Payments" component={PaymentScreen} />
           <Tab.Screen name="Maintenance" component={MissingScreen} />
           <Tab.Screen name="Account" component={Account} />
         </Tab.Navigator>
