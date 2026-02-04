ebbfa0b2d5bde3212991c50167f5c6fc4cf87089
ikennot <domdomkenneth23@gmail.com>
2026-02-04 21:25:15 +0800

    add uselogin.js

--- app/App.jsx
+++ app/App.jsx
@@ -1,6 +1,6 @@
 import { NavigationContainer } from "@react-navigation/native";
 import MainLayout from "./MainLayout";
-
+import { useLogin } from "./hooks/uselogin";
 export default function App() {
   return (
       <MainLayout />
